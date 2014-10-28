6aa7e3c95d9ab1bdaf596f373efef9fa9f8c2079
Aluno <Aluno@10.1.20.35>
2014-10-28 13:15:33 -0200

    Aula 32 - Template escolhido. Closes #2

--- layout/js/medidas.js
+++ layout/js/medidas.js
@@ -1,9 +1,15 @@
+/* 
+ * medidas.js
+ */
+
 var spanLargura = document.getElementById('largura');
 var spanAltura = document.getElementById('altura');
 
 window.onresize = function(){
     altura = window.innerHeight;
-     largura = window.innerHeight;
-     spanLargura.innerHTML = largura + 'px';
-     spanAltura.innerHTML = altura + 'px';
+    largura = window.innerWidth;
+    
+    spanLargura.innerHTML = largura + 'px ';
+    spanAltura.innerHTML = altura + 'px ';
+    
 };
